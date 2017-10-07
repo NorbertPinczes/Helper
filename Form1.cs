ab37eb6229027746ac7ccf33427eba11bc3b2c4a
NorbertPinczes <norbert.pinczes@yahoo.com>
2017-10-07 14:04:35 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JsonSerializerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadJson_Click(object sender, EventArgs e)
+        {
+            //step 1 open file location
+            //step 2 select json file
+            //step 3 open json file
+
+            string filePath = "";
+
+            DialogResult dResult = openJsonFileDialog.ShowDialog();
+            if (dResult == DialogResult.OK)
+                filePath = openJsonFileDialog.FileName;
+
+            try
+            {
+                ReadJsonFile(filePath);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void ReadJsonFile(string path)
+        {
+            //Open the file              
+            var stream = File.OpenText(path);
+            //Read the file              
+            string st = stream.ReadToEnd();
+
+            DisplayJson.Text = st;
+        }
+    }
+}
---
+++ JsonObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerializerApp
+{
+    public class JsonTask
+    {
+        public JsonObject tasks { get; set; }
+    }
+
+    public class JsonObject
+    {
+        public string topic { get; set; }
+        public string title { get; set; }
+    }
+}
